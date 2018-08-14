ea13499764c2db859754443c71b29c96d65faec6
Caleb Smith <karagenit@outlook.com>
2018-08-14 08:33:28 -0400

    Plot 775pro

--- graph.r
+++ graph.r
@@ -2,26 +2,32 @@
 
 cim <- read.csv("cim.csv", header = TRUE)
 mini <- read.csv("minicim.csv", header = TRUE)
+pro <- read.csv("775.csv", header = TRUE)
 
 cim$Power = cim$Power * 2
 mini$Power = mini$Power * 3
+pro$Power = pro$Power * 3
 
 x11()
 plot(cim$Time,
      cim$Power,
      col="red",
-     ylim = c(0, 800),
+     ylim = c(0, 1100),
      type="l",
      xlab="Time (s)",
      ylab="Power (W)",
-     main="Stall Power - 2 CIM vs. 3 MiniCIM")
+     main="Stall Power - 2 CIM vs. 3 MiniCIM vs 3 775pro")
 points(mini$Time,
        mini$Power,
        col="blue",
        type="l")
+points(pro$Time,
+       pro$Power,
+       col="green",
+       type="l")
 legend(x = "bottomright",
-       legend = c("CIM", "MiniCIM"),
-       col = c("red", "blue"),
+       legend = c("CIM", "MiniCIM", "775pro"),
+       col = c("red", "blue", "green"),
        lty = 1,
        bty = "n",
        cex = 0.8)
