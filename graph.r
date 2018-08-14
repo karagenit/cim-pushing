9216baf33a91f639406176e31247bc3da07b3b8c
Caleb Smith <karagenit@outlook.com>
2018-08-14 08:24:31 -0400

    Add Legend & Title

--- graph.r
+++ graph.r
@@ -7,8 +7,23 @@ cim$Power = cim$Power * 2
 mini$Power = mini$Power * 3
 
 x11()
-plot(cim$Time, cim$Power, col="red", ylim = c(0, 800), type="l",
-     xlab="Time (s)", ylab="Power (W)")
-points(mini$Time, mini$Power, col="blue", type="l")
+plot(cim$Time,
+     cim$Power,
+     col="red",
+     ylim = c(0, 800),
+     type="l",
+     xlab="Time (s)",
+     ylab="Power (W)",
+     main="Stall Power - 2 CIM vs. 3 MiniCIM")
+points(mini$Time,
+       mini$Power,
+       col="blue",
+       type="l")
+legend(x = "bottomright",
+       legend = c("CIM", "MiniCIM"),
+       col = c("red", "blue"),
+       lty = 1,
+       bty = "n",
+       cex = 0.8)
 
 Sys.sleep(1000)
