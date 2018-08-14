ef031446661f261a1fd33d541af5b1571e87c6e4
Caleb Smith <karagenit@outlook.com>
2018-08-14 08:20:06 -0400

    Scale CIM & MiniCIM

--- graph.r
+++ graph.r
@@ -3,8 +3,11 @@
 cim <- read.csv("cim.csv", header = TRUE)
 mini <- read.csv("minicim.csv", header = TRUE)
 
+cim$Power = cim$Power * 2
+mini$Power = mini$Power * 3
+
 x11()
-plot(cim$Time, cim$Power, col="red", ylim = c(0, 400), type="l",
+plot(cim$Time, cim$Power, col="red", ylim = c(0, 800), type="l",
      xlab="Time (s)", ylab="Power (W)")
 points(mini$Time, mini$Power, col="blue", type="l")
 
